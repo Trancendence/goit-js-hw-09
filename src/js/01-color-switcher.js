e45c80ad2193226618e1ba632e669d7c12e6c230
Transcendence <109620661+Trancendence@users.noreply.github.com>
2022-12-29 18:46:29 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,23 +4,14 @@ function getRandomHexColor() {
   
   const changeButton = document.querySelector('button.change-color')
   const body = document.querySelector('body')
-
+  const stopButton = document.querySelector('button.stop')
   
   changeButton.addEventListener('click', e => {
-    let color = getRandomHexColor();
-    body.style.backgroundColor = color;
-
+    changeButton.attr('disabled', true); 
+    setInterval(() => {
+        let color = getRandomHexColor();
+        body.style.backgroundColor = color;
+    }, 1000);
+ 
+    })
     
-  })
-
-
-
-
-
-
-
-
-
-// function getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-//   }
