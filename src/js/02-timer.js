4f7e10a5bfe37cf5983d082027a28881d0cfdb4e
Transcendence <109620661+Trancendence@users.noreply.github.com>
2023-01-06 17:14:57 +0200

    upd

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -23,7 +23,10 @@ console.log('startTime', startTime);
 setInterval(() => {
     const currentTime = Date.now();
     const ms = selectedDates - Date.now();
-    const timeComponents = convertMs(ms)
+    const timeComponents = convertMs(ms);
+    if (timeComponents < 1000) {
+        setInterval = 0;
+    }
     console.log('timeComponents:', timeComponents);
 }, 1000);
 
@@ -55,7 +58,4 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
   }
   
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
+ 
