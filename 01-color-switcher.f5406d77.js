d2bd42c3ec2684fe35520f5218a7374d60aaee8e
Trancendence <109620661+Trancendence@users.noreply.github.com>
2023-01-07 20:05:21 +0000

    Deploying to gh-pages from @ Trancendence/goit-js-hw-09@8496f6e8e63177707e34045b6c074a8ac3aa885e 🚀

--- 01-color-switcher.f5406d77.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button.change-color"),o=document.querySelector("body");document.querySelector("button.stop");t.addEventListener("click",(function(e){t.attr("disabled",!0),setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));o.style.backgroundColor=t}),1e3)}))}();
-//# sourceMappingURL=01-color-switcher.f5406d77.js.map
